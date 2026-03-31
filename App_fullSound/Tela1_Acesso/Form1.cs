fc5bef131a6b7a987257d7bc752fc6b23d485a53
BRUNO DUARTE MONTEIRO <bruno.dmonteiro1@senacsp.edu.br>
2026-03-30 22:07:55 -0300

    oi

--- App_fullSound/Tela1_Acesso/Form1.cs
+++ App_fullSound/Tela1_Acesso/Form1.cs
@@ -6,5 +6,25 @@ namespace Tela1_Acesso
         {
             InitializeComponent();
         }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
